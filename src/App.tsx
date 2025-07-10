d680845c69d7e82a200bd788f213a5c49d6b9c6f
ChrisAlbeaDev <chris.albea@rewst.io>
2025-07-11 06:44:02 +0800

    Initial Commit : React App with Tailwind and Typescript

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+       <h1 className="text-5xl font-bold text-red-600 p-6 rounded-lg shadow-lg bg-white">
+         Hello Tailwind!
+       </h1>
+     </div>
+  
+    </>
+  )
+}
+
+export default App
